c5295f07e5fc16e16d5dff7074f3305d74d5f8f4
Jefferson Silva <jeffer.leo93@gmail.com>
2018-08-20 12:11:57 -0600

    Quitar curso asignado y cambiar permisos

--- models/res_partner.py
+++ models/res_partner.py
@@ -10,15 +10,16 @@ class cursos_res_partner(models.Model):
     historial_curso_ids = fields.One2many('cursos.historial','alumno_id','Historial')
     historial_congelado_ids = fields.One2many('cursos.congelamiento','alumno_id','Congelamientos')
     asistencia_curso_ids = fields.One2many('cursos.asistencia','alumno_id','Asistencia')
-    curso_asignado = fields.Many2one('cursos.horario',compute='_get_curso_asignado',strin="Curso")
+    curso_asignado = fields.Many2one('cursos.horario',compute='_get_curso_asignado',string="Curso asignado")
 
     type = fields.Selection(default='other')
 
     def _get_curso_asignado(self):
         horario_id = 0
         for cliente in self:
-        	for linea in cliente.historial_curso_ids:
-        		if linea.fecha_fin == False:
-        			cliente.curso_asignado = linea.horario_id
+            for linea in cliente.historial_curso_ids:
+                if linea.fecha_fin == False:
+                    horario_id = linea.horario_id
+                    cliente.curso_asignado = linea.horario_id
 
-
+                    
